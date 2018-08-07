6ecc0c4cb77ba8512c2c02c3f53acc02f8621dd4
Justina <justinachen09@gmail.com>
2018-08-07 00:37:24 -0700

    fixed scroll and keyboard display and changed UI

--- MoodLogger/CalendarViewController.swift
+++ MoodLogger/CalendarViewController.swift
@@ -206,7 +206,7 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
             myCustomCell.entry2.alpha = 0
             myCustomCell.entry3.alpha = 0
             myCustomCell.entry4.alpha = 0
-            myCustomCell.plusIcon.alpha = 0
+//            myCustomCell.plusIcon.alpha = 0
         case 1:
             //case 1: update the first line with the color of the first entry, and hide all other lines
             let firstEntry = entries[0]
@@ -217,7 +217,7 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
             myCustomCell.entry3.alpha = 0
             myCustomCell.entry4.alpha = 0
             
-            myCustomCell.plusIcon.alpha = 0
+//            myCustomCell.plusIcon.alpha = 0
         case 2:
             myCustomCell.entry1.alpha = 1
             let firstEntry = entries[0]
@@ -231,7 +231,7 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
             myCustomCell.entry3.alpha = 0
             myCustomCell.entry4.alpha = 0
             
-            myCustomCell.plusIcon.alpha = 0
+//            myCustomCell.plusIcon.alpha = 0
         case 3:
             myCustomCell.entry1.alpha = 1
             let firstEntry = entries[0]
@@ -249,7 +249,7 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
             myCustomCell.entry3.backgroundColor = thirdColor
             myCustomCell.entry4.alpha = 0
             
-            myCustomCell.plusIcon.alpha = 0
+//            myCustomCell.plusIcon.alpha = 0
         case 4:
             myCustomCell.entry1.alpha = 1
             let firstEntry = entries[0]
@@ -271,30 +271,32 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
             let fourthColor = fourthEntry.mood.colorValue
             myCustomCell.entry4.backgroundColor = fourthColor
             
-            myCustomCell.plusIcon.alpha = 0
-        default: //unhide the plus icon
-            myCustomCell.entry1.alpha = 1
-            let firstEntry = entries[0]
-            let firstColor = firstEntry.mood.colorValue
-            myCustomCell.entry1.backgroundColor = firstColor
-            
-            myCustomCell.entry2.alpha = 1
-            let secondEntry = entries[1]
-            let secondColor = secondEntry.mood.colorValue
-            myCustomCell.entry2.backgroundColor = secondColor
-            
-            myCustomCell.entry3.alpha = 1
-            let thirdEntry = entries[2]
-            let thirdColor = thirdEntry.mood.colorValue
-            myCustomCell.entry3.backgroundColor = thirdColor
-            
-            myCustomCell.entry4.alpha = 1
-            let fourthEntry = entries[3]
-            let fourthColor = fourthEntry.mood.colorValue
-            myCustomCell.entry4.backgroundColor = fourthColor
-            
-            //show the plus icon
-            myCustomCell.plusIcon.alpha = 1
+//            myCustomCell.plusIcon.alpha = 0
+
+        default:
+            break
+//            myCustomCell.entry1.alpha = 1
+//            let firstEntry = entries[0]
+//            let firstColor = firstEntry.mood.colorValue
+//            myCustomCell.entry1.backgroundColor = firstColor
+//
+//            myCustomCell.entry2.alpha = 1
+//            let secondEntry = entries[1]
+//            let secondColor = secondEntry.mood.colorValue
+//            myCustomCell.entry2.backgroundColor = secondColor
+//
+//            myCustomCell.entry3.alpha = 1
+//            let thirdEntry = entries[2]
+//            let thirdColor = thirdEntry.mood.colorValue
+//            myCustomCell.entry3.backgroundColor = thirdColor
+//
+//            myCustomCell.entry4.alpha = 1
+//            let fourthEntry = entries[3]
+//            let fourthColor = fourthEntry.mood.colorValue
+//            myCustomCell.entry4.backgroundColor = fourthColor
+//
+//            //show the plus icon
+//            myCustomCell.plusIcon.alpha = 1
         }
     }
     
--- MoodLogger/NotesViewController.swift
+++ MoodLogger/NotesViewController.swift
@@ -17,12 +17,17 @@ class NotesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
+        
+//        Listen for keyboard events
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        didAnswerView.isUserInteractionEnabled = true
-        changeAnswerView.isUserInteractionEnabled = true
+//        didAnswerView.isUserInteractionEnabled = true
+//        changeAnswerView.isUserInteractionEnabled = true
         if let mood = entry {
             didAnswerView.text = mood.answer1
             changeAnswerView.text = mood.answer2
@@ -31,7 +36,54 @@ class NotesViewController: UIViewController {
             changeAnswerView.text = ""
         }
     }
-
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    /** check if the view is pushed up or not */
+    var isViewElevated: Bool {
+        return view.frame.origin.y != 0
+    }
+    
+    private var keyboardRect: CGRect!
+    
+    func pushUpViewIfNeeded() {
+        if isViewElevated == false {
+            UIView.animate(withDuration: 0.15) {
+                self.view.frame.origin.y = -self.keyboardRect.height + 64
+            }
+        }
+    }
+    
+    func pushDownViewIfNeeded() {
+        if isViewElevated {
+            UIView.animate(withDuration: 0.15) {
+                self.view.frame.origin.y = 0
+            }
+        }
+    }
+    
+    
+    @objc func keyboardWillChange(notification: Notification) {
+        print("Keyboard will show: \(notification.name.rawValue)")
+        
+        guard let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        self.keyboardRect = keyboardRect
+        
+        if notification.name == Notification.Name.UIKeyboardWillShow ||
+            notification.name == Notification.Name.UIKeyboardWillChangeFrame {
+//            pushUpViewIfNeeded()
+        } else {
+            pushDownViewIfNeeded()
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier else { return }
         
@@ -80,6 +132,16 @@ class NotesViewController: UIViewController {
         
     }
 
+}
+
+extension NotesViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView === self.didAnswerView {
+            pushDownViewIfNeeded()
+        } else if textView === self.changeAnswerView {
+            pushUpViewIfNeeded()
+        }
+    }
 }
 
 extension NotesViewController {
